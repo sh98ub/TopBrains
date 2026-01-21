4fdf4794cc7ce5064272ef4e9134fdf7b5d0ac71
shubham <shubham@gmail.com>
2026-01-21 12:12:53 +0530

    Dictionary

---
+++ Dictionary/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+class Program
+{
+  public static void Main()
+  {
+    Dictionary<int,int> employeedetails=new Dictionary<int, int>()
+    {
+      {101,1000},
+      {102,2000},
+      {103,3000},
+      {104,4000},
+      {105,5000},
+      {106,6000}
+      
+    };
+
+    List<int> list=new List<int>()
+    {
+      101,102,103
+    };
+    int sum=0;
+
+    foreach (var item in list)
+    {
+      if (employeedetails.ContainsKey(item))
+      {
+        sum+=employeedetails[item];
+      }
+      
+    }
+    Console.WriteLine(sum);
+  }
+}
