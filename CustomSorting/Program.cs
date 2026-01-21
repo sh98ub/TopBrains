0718ee84ad2e8972a78a8916f6cdcbae877e20a3
shubham <shubham@gmail.com>
2026-01-21 11:50:27 +0530

    add

---
+++ CustomSorting/Program.cs
@@ -0,0 +1,41 @@
+﻿// // See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+
+using System.Security.Cryptography;
+
+public class Student
+{
+  public string Name { get; set; }
+  public int Age { get; set; }
+
+  public double Marks { get; set; }
+
+  
+}
+
+class Program
+{
+
+  public static void Main()
+  {
+      List<Student> students=new List<Student>()
+    {
+     new Student {Name="Shubham",Age=20,Marks=40},
+      new Student{Name="Rohit",Age=23,Marks=45},
+     new Student {Name="Harsh",Age=22,Marks=42},
+     new Student {Name="Ravi",Age=21,Marks=42},
+     new Student {Name="Raj",Age=23,Marks=40}
+    };
+    
+
+    var num=students.OrderBy(n=>n.Marks).ThenBy(n=>n.Age).ToList();
+
+
+    foreach (Student item in num)
+    {
+      Console.WriteLine($"Name {item.Name} age id {item.Age} and marks is {item.Marks}");
+      
+    }
+  }
+}
