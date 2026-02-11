bfe6a9cea1d2182ce329f8d4a93a77b7e00262b5
shubham <shubham@gmail.com>
2026-02-11 12:31:44 +0530

    commit

---
+++ Word/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class Program
+{
+  public static void Main()
+  {
+    Console.WriteLine("Enter the string");
+    string st=Console.ReadLine();
+    string[] arr=st.Split(' ',StringSplitOptions.RemoveEmptyEntries);
+
+    Array.Reverse(arr);
+
+   // Dictionary<char,int> word=new Dictionary<char, int>();
+
+   foreach (var item in arr)
+   {
+    Console.Write(item+" ");
+    
+   }
+
+
+    // foreach (var c in st.ToLower())
+    // {
+    //   if(c==' ')
+    //   {
+    //     continue;
+    //   }
+    //   if (word.ContainsKey(c))
+    //   {
+    //     word[c]++;
+    //   }
+    //   else
+    //   {
+    //     word[c]=1;
+    //   }
+    // }
+
+    // foreach (var item in word)
+    // {
+    //   Console.WriteLine($"{item.Key}--{item.Value}");
+      
+    // }
+  }
+}
