d48fdaf524e78a81ffdb4f925bf63d2a53f605cb
shubham <shubham@gmail.com>
2026-02-24 21:25:20 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ParameterizedQuery
+{
+    class Program
+    {
+        static void Main()
+        {
+            Parameter obj = new Parameter();
+
+            Console.Write("Enter Student Id: ");
+            int id = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter Name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Enter Marks: ");
+            int marks = int.Parse(Console.ReadLine());
+
+            obj.AddNewStudent(id, name, marks);
+
+            Console.ReadLine(); // Optional pause
+        }
+    }
+}
