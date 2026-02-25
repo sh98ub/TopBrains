202ba7eeef2c0ff5757aaf71e6aedf8412f4c7cd
Shubham Gupta <120040737+sh98ub@users.noreply.github.com>
2026-02-25 23:31:22 +0530

    Add files via upload

---
+++ DuplicateProduct/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class Program
+{
+  public static void Main()
+  {
+            var input = new List<string> { "Pen", "Book", "Pen", "Pencil", "Book" };
+
+            var res=input.GroupBy(x=>x).Where(g=>g.Count()>1).Select(g=>g.Key).ToList();
+
+            foreach (var item in res)
+            {
+              Console.WriteLine(item);
+              
+            }
+
+  }
+  
+}
