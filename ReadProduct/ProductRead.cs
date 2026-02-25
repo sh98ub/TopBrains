cc4956033aad6fbd647ad0379afbc732a661fed5
Shubham Gupta <120040737+sh98ub@users.noreply.github.com>
2026-02-25 23:10:16 +0530

    Add files via upload
    commit

---
+++ ReadProduct/ProductRead.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadProduct
+{
+    internal class ProductRead
+    {
+        string coneectionstring = "Data Source=PARADOX\\SQLEXPRESS;Initial Catalog=AdventureWorks2022;Integrated Security=True;";
+
+        public List<Product> GetProducts()
+        {
+            List<Product> products = new List<Product>();
+
+            SqlConnection connection = new SqlConnection(coneectionstring);
+            connection.Open();
+            string query = "Select ProductID,Name,ListPrice from Production.Product";
+            SqlCommand cmd = new SqlCommand(query, connection);
+
+            SqlDataReader sqlDataReader = cmd.ExecuteReader();
+
+            while (sqlDataReader.Read())
+            {
+                products.Add(new Product
+                {
+                    ProductID = (int)sqlDataReader["ProductID"],
+                    Name = sqlDataReader["Name"].ToString(),
+                    ListPrice = (decimal)sqlDataReader["ListPrice"]
+
+
+                });
+            }
+            connection.Close();
+
+            return products;
+
+
+        }
+    }
+}
